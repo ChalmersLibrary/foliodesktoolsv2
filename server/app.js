fe05382387bf86d4f214d4736500852190e28c24
Lari Kovanen <lari.kovanen@chalmers.se>
2026-02-05 16:47:42 +0100

    Only from Chalmers IP.

--- server/app.js
+++ server/app.js
@@ -10,6 +10,14 @@ app.use(express.urlencoded({extended: true}))
 
 app.use(cookieParser())
 
+app.use((req, res, next) => {
+  if (req.ip.includes('127.0.0.1') || req.ip.includes('::1') || (req.get('X-Forwarded-For') != undefined && req.get('X-Forwarded-For').includes('129.16'))) {
+    next()
+  } else {
+    res.status(403).sendFile(`${__dirname}/403.html`)
+  }
+})
+
 app.use('/api', api)
 app.use(express.static('public'))
 
