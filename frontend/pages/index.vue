cef6c6e39bfad81da38a168853281f5965742af2
Lari Kovanen <lari.kovanen@chalmers.se>
2026-03-10 16:14:53 +0100

    Stop redirecting the index page to the last one used.

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,13 +4,4 @@
 </template>
 
 <script lang="ts" setup>
-  const token = useCookie('token');
-
-  // Redirect authenticated users to their last visited page after PWA restart
-  if (import.meta.client && token.value) {
-    const lastRoute = localStorage.getItem('lastRoute');
-    if (lastRoute && lastRoute !== '/') {
-      navigateTo(lastRoute);
-    }
-  }
 </script>
