c29f98b84ac76ffd557d305129ff25c61a8c0c20
Lari Kovanen <lari.kovanen@chalmers.se>
2026-03-10 15:34:52 +0100

    env var consistency

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 const PORT = process.env.PORT || 3000
-const devel = process.env.node_env == 'development'
+const devel = process.env.NODE_ENV == 'development'
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
