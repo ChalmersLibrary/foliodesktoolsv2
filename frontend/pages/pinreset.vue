a6edd210c952863d0929df91e6b4c04a85a6259f
Lari Kovanen <lari.kovanen@chalmers.se>
2026-06-15 13:09:36 +0200

    Show error when pins don't match and they are the correct length.

--- frontend/pages/pinreset.vue
+++ frontend/pages/pinreset.vue
@@ -12,14 +12,14 @@
       <div>Email: {{user.personal.email}}</div>
       <div>External system id: {{user.externalSystemId}}</div>
       <div v-if="user.pwdReset && user.active">
-        <label for="pin1">PIN-kod</label>
-        <input v-model="pin1" type="password" autocomplete="off">
+        <label for="pin1">PIN-kod (6 siffror)</label>
+        <input v-model="pin1" class="masked-input" autocomplete="off" maxlength="6">
         <br>
         <label for="pin2">PIN-kod igen</label>
-        <input v-model="pin2" type="password" autocomplete="off" v-on:keyup.enter="changePin">
-        <!-- <div v-if="pinOk">Match</div> -->
+        <input v-model="pin2" class="masked-input" autocomplete="off" v-on:keyup.enter="changePin"  maxlength="6">
+        <br />
         <button v-if="pinOk" @click="changePin">Byt PIN-kod</button>
-        <div v-else>Sex siffror.</div>
+        <div v-else-if="pin1 && pin1.length == 6 && pin2 && pin2.length == 6">Pinkoderna är inte lika.</div>
       </div>
       <br>
       <div v-if="pinMessageOk" class="pinMessageOk">{{pinMessageOk}}</div>
@@ -105,5 +105,8 @@
   }
   .pinMessage {
     color: red;
+  }
+  .masked-input {
+    -webkit-text-security: disc;
   }
 </style>
