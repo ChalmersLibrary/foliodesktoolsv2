2eda485d82a314ad0a3881bd1d00e8020970beaf
MarieWi <52502488+MarieWi@users.noreply.github.com>
2026-08-11 13:57:52 +0200

    Update MainInfo.vue
    Uppdaterat texten

--- frontend/components/MainInfo.vue
+++ frontend/components/MainInfo.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
     <h2>Välkommen kollega på Chalmers bibliotek!</h2>
-    <p><strong>Inloggningsuppgifter finns i Infodiskmanualen</strong></p>
-    <p>Här kan du rensa reservationshyllan, skapa framplockningslistor och byta PIN-kod.<br>
-    Du måste vara på Chalmers nätverk. Diskappen fungerar även på VPN, men inte Always on VPN.</p>
+    <p>Här kan du rensa reservationshyllan, skapa framplockningslistor, byta PIN-kod och skapa nya bibliotekskonton.<br>
     <br><br>
     <h3>Lägg diskappen som en app på din dator - Microsoft Edge och Chrome</h3>
     <p>I URL-fältet finns en ikon. Klicka på den så installeras appen och du kan lägga den i verktygsfältet på datorn.</p>
     <p>Edge-ikonen visas nedan, i Chrome liknar den en dator med pil nedåt.</p>
     <img src="/img/Install-web-app-icon-edge.png">
-    <h3>Att använda diskappen på mobilen</h3>
+    <h3>Använda diskappen på mobilen</h3>
     <p>Sök upp https://foliodiskapp.lib.chalmers.se/<br>
-    Lägg gärna länken på startskärmen. <br>
-    Liggande skärm fungerar bäst.<br>
+    Lägg länken på startskärmen. <br>
     Iphone: Klicka på dela länk i verktygsfältet och sedan "Lägg till på hemskärmen"</p>
   </div>
 </template>
