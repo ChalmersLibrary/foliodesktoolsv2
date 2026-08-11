f2f88c7dfca4e49f1e490caf37c1f67a8f35cdc6
MarieWi <52502488+MarieWi@users.noreply.github.com>
2026-08-11 16:13:30 +0200

    Tagit bort en annan punkt

--- frontend/components/MainInfo.vue
+++ frontend/components/MainInfo.vue
@@ -3,7 +3,7 @@
     <h2>Välkommen kollega på Chalmers bibliotek!</h2>
     <p>Här kan du rensa reservationshyllan, skapa framplockningslistor, byta PIN-kod och skapa nya bibliotekskonton.</p>
     <br>
-    <h3>Lägg diskappen som en app på din dator.</h3>
+    <h3>Lägg diskappen som en app på din dator</h3>
     <p>Sök upp https://foliodiskapp.lib.chalmers.se/</p>
     <p>I URL-fältet finns en app-ikon längst till höger. Klicka på den så installeras appen och du kan lägga den i verktygsfältet på datorn.</p>
     <p>I Chrome liknar den en dator med pil nedåt. Edge-ikonen visas nedan.</p>
