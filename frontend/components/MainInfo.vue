27aefa4dca3ae30eaaf95af489f2f9a9abdda834
MarieWi <52502488+MarieWi@users.noreply.github.com>
2026-08-11 14:03:27 +0200

    Update MainInfo.vue
    Uppdaterat lite till

--- frontend/components/MainInfo.vue
+++ frontend/components/MainInfo.vue
@@ -3,9 +3,10 @@
     <h2>Välkommen kollega på Chalmers bibliotek!</h2>
     <p>Här kan du rensa reservationshyllan, skapa framplockningslistor, byta PIN-kod och skapa nya bibliotekskonton.<br>
     <br><br>
-    <h3>Lägg diskappen som en app på din dator - Microsoft Edge och Chrome</h3>
-    <p>I URL-fältet finns en ikon. Klicka på den så installeras appen och du kan lägga den i verktygsfältet på datorn.</p>
-    <p>Edge-ikonen visas nedan, i Chrome liknar den en dator med pil nedåt.</p>
+    <h3>Lägg diskappen som en app på din dator.</h3>
+    <p>Sök upp https://foliodiskapp.lib.chalmers.se/<br>
+    <p>I URL-fältet finns en app-ikon längst till höger. Klicka på den så installeras appen och du kan lägga den i verktygsfältet på datorn.</p>
+    <p>I Chrome liknar den en dator med pil nedåt. Edge-ikonen visas nedan.</p>
     <img src="/img/Install-web-app-icon-edge.png">
     <h3>Använda diskappen på mobilen</h3>
     <p>Sök upp https://foliodiskapp.lib.chalmers.se/<br>
